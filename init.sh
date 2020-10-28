0f89582cbe6fc2c9afa315dab1abb25642f58a61
CryptKid <CryptKid@users.noreply.github.com>
2020-10-28 17:50:05 +0100

    Fix init.sh (No comment above shebang)
    I put a comment above the shebang
    In consequence it caused an exec format error

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
-#for normal deployment ignore this file, it is only useful for docker deployment
 #!/bin/sh
+
+#for normal deployment ignore this file, it is only useful for docker deployment
 pip install requests
 pip install feedparser
 python /opt/repo/mi-bot.py
