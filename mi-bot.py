ed59703b77beeee31bce23f82b926267c9dff295
Christian Stemmle <chstem@e-mail.de>
2020-11-13 21:21:30 +0100

    add first command to bot

--- mi-bot.py
+++ mi-bot.py
@@ -1,23 +1,25 @@
 #!/usr/bin/env python3
 
-import json
 import os
 import urllib
+import random
 import re
 from time import mktime, sleep
 from datetime import datetime as dt
 
 import feedparser
 import requests
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, CallbackContext
 
 
 # Read bot token from environment
 TOKEN = os.environ['MIA_TG_TOKEN']
 CHATID = os.environ['MIA_TG_CHATID']
-
 URL = f"https://api.telegram.org/bot{TOKEN}/"
 
 
+# code not using python-telegram-bot library
 def get_url(url):
     response = requests.get(url)
     content = response.content.decode("utf8")
@@ -68,7 +70,34 @@ def check_youtube(max_age=3600):
                 f'[Jetzt ansehen]({newest_episode.link})')
 
 
-while True:
-    check_minkorrekt(3600)
-    check_youtube(3600)
-    sleep(3595)
+def get_episode_titles():
+    MINKORREKT_RSS = 'http://minkorrekt.de/feed/mp3'
+    mi_feed = feedparser.parse(MINKORREKT_RSS)
+    return [i.title for i in mi_feed['items']]
+
+
+MINKORREKT_TITLES = get_episode_titles()
+
+
+def feed_loop():
+    while True:
+        check_minkorrekt(3600)
+        check_youtube(3600)
+        sleep(3595)
+
+
+# python-telegram-bot library
+def cookie(update: Update, context: CallbackContext) -> None:
+    text = random.choice(MINKORREKT_TITLES)
+    update.message.reply_text(f'\U0001F36A {text} \U0001F36A', quote=False)
+
+
+updater = Updater(TOKEN)
+
+updater.dispatcher.add_handler(CommandHandler('keks', cookie))
+
+
+if __name__ == '__main__':
+    updater.start_polling()
+    feed_loop()
+    updater.idle()
