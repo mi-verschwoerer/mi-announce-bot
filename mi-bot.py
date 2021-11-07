03e0a0ac250608e3520735c1370aced01ccb6bce
Christian Stemmle <chstem@e-mail.de>
2021-11-07 11:36:19 +0100

    implement a class PodcastFeed to manage and cache the RSS feed

--- mi-bot.py
+++ mi-bot.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
 import os
-import urllib
+import pickle
 import random
 import re
-from time import mktime, sleep
+import time
+import traceback
+import urllib
 from datetime import datetime as dt
 from subprocess import run
 
@@ -20,8 +22,73 @@ from telegram.ext import Updater, CommandHandler, CallbackContext
 TOKEN = os.environ['MIA_TG_TOKEN']
 CHATID = os.environ['MIA_TG_CHATID']
 URL = f"https://api.telegram.org/bot{TOKEN}/"
+DUMP = os.getenv('MIA_DUMP', '')
 DIRNAME = os.path.dirname(os.path.realpath(__file__))
 MINKORREKT_RSS = 'http://minkorrekt.de/feed/mp3'
+
+
+class PodcastFeed:
+    """Represents the parsed and cached podcast RSS feed"""
+
+    def __init__(self, url: str, max_age: int = 3600, dump: str = ''):
+        """
+        :param url: URL of the feed to be parsed.
+        :param max_age: Time in seconds how long the parsed feed is consider valid.
+                        The feed will be refreshed automatically on the first access
+                        after max_age is passed.
+        :param dump: Allows local storage of the feed. If a valid file path, the feed
+                     object will be stored there after refresh, and reloaded on
+                     re-initialization. Primarily intended to speed loading time for debugging.
+        """
+        self.url = url
+        self.max_age = max_age
+        self.dump = dump
+
+        if dump and os.path.isfile(dump):
+            try:
+                with open(dump, 'rb') as f:
+                    self.last_updated, self.feed = pickle.load(f)
+                print(time.asctime(), 'Reloaded dumped feed')
+            except Exception as exc:
+                print(f'{exc!r}\n{traceback.format_exc()}')
+                print(time.asctime(), 'Failed loding dumped feed. Falling back to download.')
+                self._get_feed()
+        else:
+            print(time.asctime(), 'Getting feed')
+            self._get_feed()
+
+    def _get_feed(self):
+        self.feed = feedparser.parse(self.url)
+        self.last_updated = time.time()
+        print(time.asctime(), 'Done parsing feed')
+        if self.dump:
+            with open(self.dump, 'wb') as f:
+                pickle.dump((self.last_updated, self.feed), f)
+
+    def refresh(self):
+        if self.last_updated + self.max_age < time.time():
+            print(time.asctime(), 'Refreshing feed')
+            self._get_feed()
+
+    def check_new_episode(self, max_age=3600):
+        latest_episode = self.latest_episode
+        episode_release = dt.fromtimestamp(time.mktime(latest_episode['published_parsed']))
+        if (dt.now() - episode_release).total_seconds() < max_age:
+            return latest_episode
+        return False
+
+    @property
+    def latest_episode(self):
+        self.refresh()
+        return self.feed['items'][0]
+
+    @property
+    def episode_titles(self):
+        self.refresh()
+        return [i.title for i in self.feed['items']]
+
+
+mi_feed = PodcastFeed(url=MINKORREKT_RSS, dump=DUMP)
 
 
 # code not using python-telegram-bot library
@@ -55,54 +122,43 @@ def tg_send(text):
 
 
 def check_minkorrekt(max_age=3600):
-    mi_feed = feedparser.parse(MINKORREKT_RSS)
-    newest_episode = mi_feed['items'][0]
-    episode_release = dt.fromtimestamp(mktime(newest_episode['published_parsed']))
-    if (dt.now() - episode_release).total_seconds() < max_age:
-        tg_send(f'*{newest_episode.title}*\n'
+    new_episode = mi_feed.check_new_episode(max_age=max_age)
+    if new_episode:
+        tg_send(f'*{new_episode.title}*\n'
                 'Eine neue Folge Methodisch inkorrekt ist erschienen\\!\n'
-                f'[Jetzt anhören]({newest_episode.link})')
+                f'[Jetzt anhören]({new_episode.link})')
 
 
 def check_youtube(max_age=3600):
     YOUTUBE_RSS = 'https://www.youtube.com/feeds/videos.xml?channel_id=UCa8qyXCS-FTs0fHD6HJeyiw'
     yt_feed = feedparser.parse(YOUTUBE_RSS)
     newest_episode = yt_feed['items'][0]
-    episode_release = dt.fromtimestamp(mktime(newest_episode['published_parsed']))
+    episode_release = dt.fromtimestamp(time.mktime(newest_episode['published_parsed']))
     if (dt.now() - episode_release).total_seconds() < max_age:
         tg_send(f'*{newest_episode.title}*\n'
                 'Eine neues Youtube Video ist erschienen!\n'
                 f'[Jetzt ansehen]({newest_episode.link})')
 
 
-def get_episode_titles():
-    mi_feed = feedparser.parse(MINKORREKT_RSS)
-    return [i.title for i in mi_feed['items']]
-
-
-MINKORREKT_TITLES = get_episode_titles()
-
-
 def feed_loop():
     while True:
         check_minkorrekt(3600)
         check_youtube(3600)
-        sleep(3595)
+        time.sleep(3595)
 
 
 # python-telegram-bot library
 def latest_episode(update: Update, context: CallbackContext) -> None:
-    mi_feed = feedparser.parse(MINKORREKT_RSS)
-    newest_episode = mi_feed['items'][0]
-    episode_release = dt.fromtimestamp(mktime(newest_episode['published_parsed'])).date()
+    latest_episode = mi_feed.latest_episode
+    episode_release = dt.fromtimestamp(time.mktime(latest_episode['published_parsed'])).date()
     datum = episode_release.strftime('%d.%m.%Y')
-    text = (f'Die letzte Episode ist *{newest_episode.title}* vom {datum}.\n'
-            f'[Jetzt anhören]({newest_episode.link})')
+    text = (f'Die letzte Episode ist *{latest_episode.title}* vom {datum}.\n'
+            f'[Jetzt anhören]({latest_episode.link})')
     update.message.reply_text(text, quote=False, parse_mode=ParseMode.MARKDOWN)
 
 
 def cookie(update: Update, context: CallbackContext) -> None:
-    text = random.choice(MINKORREKT_TITLES)
+    text = random.choice(mi_feed.episode_titles)
     update.message.reply_text(f'\U0001F36A {text} \U0001F36A', quote=False)
 
 
@@ -125,18 +181,14 @@ def fuzzy_topic_search(update: Update, context: CallbackContext) -> None:
     i = update.message.text.find(' ')
     if i > 0:
         search_term = update.message.text[i+1:]
-    MINKORREKT_RSS = 'http://minkorrekt.de/feed/mp3'
-    mi_feed = feedparser.parse(MINKORREKT_RSS)
-    topics_all_episodes = [[i.title, i.content[0].value.replace("<!-- /wp:paragraph -->", "").replace("<!-- wp:paragraph -->", "")] for i in mi_feed.entries]
+    topics_all_episodes = [[i.title, i.content[0].value.replace("<!-- /wp:paragraph -->", "").replace("<!-- wp:paragraph -->", "")] for i in mi_feed.feed.entries]
     ratios = process.extract(search_term, topics_all_episodes)
     episodes = [ratio[0][0] for ratio in ratios[:3]]
     text = "Die besten 3 Treffer sind die Episoden:\n" + "\n".join(episodes)
     update.message.reply_text(text, quote=False, parse_mode=ParseMode.MARKDOWN)
 
 def topics_of_episode(update: Update, context: CallbackContext) -> None:
-    MINKORREKT_RSS = 'http://minkorrekt.de/feed/mp3'
-    mi_feed = feedparser.parse(MINKORREKT_RSS)
-    topics_all_episodes = [[i.title, i.content[0].value.replace("<!-- /wp:paragraph -->", "").replace("<!-- wp:paragraph -->", "")] for i in mi_feed.entries]
+    topics_all_episodes = [[i.title, i.content[0].value.replace("<!-- /wp:paragraph -->", "").replace("<!-- wp:paragraph -->", "")] for i in mi_feed.feed.entries]
     i = update.message.text.find(' ')
     if i > 0:
         episode_number = update.message.text[i+1:]
@@ -168,8 +220,6 @@ def topics_of_episode(update: Update, context: CallbackContext) -> None:
     episode_title = "12a Du wirst wieder angerufen! & 12b Previously (on) Lost" if episode_number == 12 else topics_all_episodes[index_number][0]
     text = f"Die Themen von Folge {episode_title} sind:\n{topics_text}"
     update.message.reply_text(text, quote=False, parse_mode=ParseMode.MARKDOWN)
-
-
 
 
 updater = Updater(TOKEN)
